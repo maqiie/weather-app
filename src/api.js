e2e08e21e6d6410f1482e3c59a1471ad7aba05b7
maqiie <candiemp2580@gmail.com>
2023-01-31 23:16:43 +0300

    completed the whole app

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "2f71081234msh633ae31698c51cfp193c26jsnfcb31b97502e",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "917a3c2811b90c49174f722be7138596"; // enter your key from openweather API
